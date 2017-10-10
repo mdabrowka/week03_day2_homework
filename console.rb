66e55379a6b01be8e4c4f1012abb73a29c72e4ac
Marta <martadabrowka@gmail.com>
2017-10-10 16:59:28 +0100

    added some instances of the class bounty

---
+++ console.rb
@@ -0,0 +1,25 @@
+require('pry')
+require_relative('models/bounty.rb')
+
+
+bounty_1 = Bounty.new(
+  {
+    'name' => 'Rose'
+    'species' => 'human'
+    'last_know_location' => 'Glasgow'
+    'favourite_weapon' => 'bare hands'
+  }
+)
+
+
+bounty_2 = Bounty.new(
+  {
+    'name' => 'Fiona'
+    'species' => 'hippo'
+    'last_know_location' => 'Zoo'
+    'favourite_weapon' => 'hippo bite'
+  }
+)
+
+binding.pry
+nil
